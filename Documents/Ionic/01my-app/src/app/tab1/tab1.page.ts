b3c38c1e87094d7b9b6e6a7a4d7ea5a06db77c75
Emanuel <herediaemanuel17@gmail.com>
2021-03-02 01:18:13 -0300

    Fin Seccion 2

---
+++ Documents/Ionic/01my-app/src/app/tab1/tab1.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(){
+    let numero = 10;
+    numero = 20;
+    numero = numero + 12;
+    numero = 0;
+    console.log(numero);
+  }
+
+}
