6c0fed2dd220c85a3137829ae0192adf0d92c8e6
Emanuel <herediaemanuel17@gmail.com>
2021-03-09 00:46:30 -0300

    refresh de archivo txt

--- Documents/Ionic/03-componentes/src/app/app-routing.module.ts
+++ Documents/Ionic/03-componentes/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
  
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  
 ];
 
 @NgModule({
