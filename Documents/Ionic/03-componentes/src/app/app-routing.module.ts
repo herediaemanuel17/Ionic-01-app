8d08cf67a7e9e31f99aef3536dbf35b3d5fe86c0
Emanuel <herediaemanuel17@gmail.com>
2021-03-09 01:44:18 -0300

    Crear paginas Alert, action-sheet e inicio y Agregar boton de regrear con sus propiedades

--- Documents/Ionic/03-componentes/src/app/app-routing.module.ts
+++ Documents/Ionic/03-componentes/src/app/app-routing.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
  
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
+
   {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full'
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
   
 ];
